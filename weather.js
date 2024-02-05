02dcb028876ab17da3c63a24fe1a193e375a743d
ShrutiGhosh24 <157016563+ShrutiGhosh24@users.noreply.github.com>
2024-02-05 19:00:12 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -25,36 +25,7 @@ function fetchWeather(location){
         loc.textContent = data.name;
         temp.textContent = `${Math.round(data.main.temp)}°C`;
         des.textContent = data.weather[0].description;
-        weatherIcon.src = `icons/${data.weather[0].icon}`;
-
-       /* if(data.weather[0].main == "clear sky"){
-            weatherIcon.src  = "icons/01d.png";
-        }
-        else if(data.weather[0].main == "few clouds"){
-            weatherIcon.src  = "weather/icons/02d.png";
-        }
-        else if(data.weather[0].main == "sacttered clouds"){
-            weatherIcon.src  = "icons/03d.png";
-        }
-        else if(data.weather[0].main == "broken clouds"){
-            weatherIcon.src  = "icons/04d.png";
-        }
-        else if(data.weather[0].main == "shower rain"){
-            weatherIcon.src  = "icons/09d.png";
-        }
-        else if(data.weather[0].main == "rain"){
-            weatherIcon.src  = "icons/10d.png";
-        }
-        else if(data.weather[0].main == "thunderstorm"){
-            weatherIcon.src  = "icons/11d.png";
-        }
-        else if(data.weather[0].main == "snow"){
-            weatherIcon.src  = "icons/13d.png";
-        }
-        else if(data.weather[0].main == "mist"){
-            weatherIcon.src  = "C:\Users\DELL\Documents\web development\weather/icons/50d.png";
-        }*/
-
+        weatherIcon.src = `${data.weather[0].icon}.png`;
         console.log(data);
        
 
